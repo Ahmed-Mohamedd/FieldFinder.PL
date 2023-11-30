5fe36e0159484427574c99278eaa6211e4a082c1
Aya Saad <ayasa3d5@gmail.com>
2023-12-01 00:22:49 +0200

    Addin IGeneericRepository

---
+++ FieldFinder.BLL/Interfaces/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FieldFinder.BLL.Interfaces
+{
+    public interface IGenericRepository<T> where T : class // crud
+    {
+
+        IEnumerable<T> GetAll(); //retrieve
+
+        void Add(T t);  //create
+
+        void Update(T t); //update
+        void Delete(T t); //d
+
+    }
+}
