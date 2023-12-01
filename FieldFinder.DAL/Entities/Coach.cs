1a181b140892eca7886762723674e48e0a8e0dea
Aya Saad <ayasa3d5@gmail.com>
2023-12-01 14:58:38 +0200

    AddingCoachController

--- FieldFinder.DAL/Entities/Coach.cs
+++ FieldFinder.DAL/Entities/Coach.cs
@@ -10,5 +10,9 @@ namespace FieldFinder.DAL.Entities
     {
         public int ID { get; set; }
         public string Name { get; set; }
+
+        public string  Email { get; set; }
+        public string  Phone { get; set; }
+        public int  YearsExperience { get; set; }
     }
 }
---
+++ FieldFinder.PL/Controllers/CoachController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FieldFinder.PL.Controllers
+{
+    public class CoachController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
